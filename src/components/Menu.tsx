1ec6494334b104ed8260de54ffd751b20289844a
Leonardo-villagran <leonardovillagran@yahoo.com>
2024-02-17 17:21:32 -0300

    menu fixed

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Menu: React.FC = () => {
+    // Estado para controlar si el menú está colapsado o expandido
+    const [isMenuCollapsed, setIsMenuCollapsed] = useState(true);
+
+    // Función para alternar entre colapsado y expandido
+    const toggleMenuCollapse = () => {
+        setIsMenuCollapsed(!isMenuCollapsed);
+    };
+
     return (
         <div className="sticky-top">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                <div className="container">
-                    <a className="navbar-brand" href="#">Cuidadoras</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className="navbar-brand ps-2" href="#">Cuidadoras</a>
+                    <button className="navbar-toggler" type="button" onClick={toggleMenuCollapse} aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className={`collapse navbar-collapse ${isMenuCollapsed ? '' : 'show'}`} id="navbarNav">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <a className="nav-link" href="#personal">Empresa</a>
+                                <a className="nav-link ps-2" href="#personal">Empresa</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#serviceTitle">Servicios</a>
+                                <a className="nav-link ps-2" href="#serviceTitle">Servicios</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#contact">Contacto</a>
+                                <a className="nav-link ps-2" href="#contact">Contacto</a>
                             </li>
                         </ul>
                     </div>
-                </div>
             </nav>
         </div>
     );
