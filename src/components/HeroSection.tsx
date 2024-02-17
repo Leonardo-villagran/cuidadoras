0733d32a56c28c870eb64c5cfbcb242ffc89d4b4
Leonardo-villagran <leonardovillagran@yahoo.com>
2024-02-17 17:25:21 -0300

    button hero fix

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import '../assets/css/HeroSection.css'; // Archivo de estilos para el componente
 
 const HeroSection: React.FC = () => {
+    const handleButtonClick = () => {
+        const serviceTitleSection = document.getElementById('serviceTitle');
+        if (serviceTitleSection) {
+            serviceTitleSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
     return (
         <div id="hero" className='container pb-4'>
-            <div className="hero-section" style={{ backgroundImage: `url('./images/01.jpg')`}}>
+            <div className="hero-section" style={{ backgroundImage: `url('./images/01.jpg')` }}>
                 <div className="hero-content">
                     <h1 className="hero-section__title">Descubre tu equipo ideal de cuidadoras</h1>
                     <p className="hero-section__subtitle">Brindando cuidado y apoyo de calidad para tus seres queridos.</p>
-                    <button className="hero-section__button">¡Empieza ahora!</button>
+                    <button className="hero-section__button" onClick={handleButtonClick}>¡Empieza ahora!</button>
                 </div>
             </div>
         </div>
