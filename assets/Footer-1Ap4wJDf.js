becf2511f102efa31a923830723b8144e39696ee
Leonardo-villagran <leonardovillagran@yahoo.com>
2024-02-20 13:03:31 -0300

    Updates

---
+++ assets/Footer-1Ap4wJDf.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DSbKch0O.js";const r=()=>e.jsxs("div",{id:"footer",className:"card-footer pt-4",children:[e.jsx("p",{children:"Dirección: Dirección de tu empresa"}),e.jsx("p",{children:"Teléfono: Teléfono de contacto"}),e.jsx("p",{children:"Email: correo@empresa.com"})]});export{r as default};
